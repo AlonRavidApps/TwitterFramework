a3da0e52a54503e11180e1f7c07c7f43e762542a
Alon Ravid <alonri75@walla.com>
2020-10-08 15:44:12 +0300

    Initial Commit

---
+++ TwitterFramework/TwitterFramework.h
@@ -0,0 +1,19 @@
+//
+//  TwitterFramework.h
+//  TwitterFramework
+//
+//  Created by Alon Ribovsky on 08/10/2020.
+//  Copyright © 2020 Alon Ribovsky. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TwitterFramework.
+FOUNDATION_EXPORT double TwitterFrameworkVersionNumber;
+
+//! Project version string for TwitterFramework.
+FOUNDATION_EXPORT const unsigned char TwitterFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TwitterFramework/PublicHeader.h>
+
+
